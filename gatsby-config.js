98a103a901a4dbfe268e4983a822f237ac3b6c4c
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-07-07 20:09:25 -0300

    starting the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,34 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Mauricio Lambiasi`,
+    siteUrl: `https://www.mauriciolambiasi.com`,
+    description: `Desenvolvimento Humano & Negócios`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production'
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+  ],
 }
