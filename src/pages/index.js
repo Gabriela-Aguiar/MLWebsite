a775a43a92ee63ce778e08a40af2fd347d1d85d1
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-07-11 13:49:33 -0300

    Header, Principal and servicos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,53 @@
 import React from "react"
-
-import { Container } from './styles'
+import { Link } from "react-router-dom"
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Header, Navbar, Principal, Serviços, Fileira, Paises } from './styles'
 
 export default function Home() {
   return (
-    <Container></Container>
+  <div>
+    <Header>
+      <h1>Mauricio Lambiasi</h1>
+      <hr/>
+      <Navbar>
+        <Router>
+          <Link to ="#">Página Inicial</Link>
+          <Link to ="#">Sobre</Link>
+          <Link to ="#">Serviços</Link>
+          <Link to ="#">Blog</Link>
+          <Link to ="#">Contato</Link>
+        </Router>
+      </Navbar>
+      <h2>Desenvolvimento Humano & Negócios</h2>
+      <p>Educação Corporativa & Orientação para a Vida</p>
+      <p>Assessoria Corporativa</p>
+    </Header>
+    <Principal>
+      <p>“Conhece-te a ti mesmo e conhecerás todo o
+      universo e os deuses, porque se o que procuras
+      não achares primeiro dentro de ti mesmo, não
+      acharás em lugar algum”
+      <br/>
+      <br/>
+      Grécia Antiga</p>
+      <img src='/images/mauricioPrincipal.jpeg'/>
+    </Principal>
+    <Serviços>
+      <h1>Serviços</h1>
+      <hr/>
+      <Fileira>
+        <img src ='/images/cv.png'/>
+        <img src ='/images/collaborative.png'/>
+        <img src ='/images/humanity.png'/>
+      </Fileira>
+      <Fileira>
+        <img src ='/images/strategy.png'/>
+        <Paises>
+          <img src ='/images/mexico.png'/>
+          <img src ='/images/brazil.png'/>
+        </Paises>
+      </Fileira>
+    </Serviços>
+  </div>
   )
 }
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,9 +1,114 @@
 import styled from 'styled-components';
 
-export const Container = styled.div `
-    padding: 0;
-    margin: 0;
-    width: 100%;
-    height: 100%;
-    background-color: aqua;
+export const Header = styled.div `
+    width:100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Roboto Condensed', sans-serif;
+
+    h1 {
+        font-size: 60px;
+        font-weight: normal;
+        color: #5DA6AD;
+    }
+
+    h2 {
+        font-size: 40px;
+        font-weight: normal;
+        margin-top: 70px;
+    }
+
+    p {
+        font-size: 22px;
+    }
+
+    hr {
+    width: 30%;
+    border: 0;
+    height: 1px;
+    background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(93, 166, 173, 0.75), rgba(0, 0, 0, 0));
+    }
 `
+
+export const Navbar = styled.div `
+    width: 100vw;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-family: 'Roboto Condensed', sans-serif;
+    margin: 50px 0;
+
+    a {
+    text-decoration: none;
+    color: #000;
+    font-size: 18px;
+    }
+
+    a:hover {
+        transform: scale(1.1);
+        border-bottom: 1px solid #5DA6AD;
+    }
+
+    hr {
+        width: 5%;
+    }
+`
+export const Principal = styled.div `
+    width: 100vw;
+    display: flex;
+    justify-content: space-around;
+    font-family: 'Roboto Condensed', sans-serif;
+    margin-top: 120px;
+
+    img {
+        width:350px;
+        border-radius: 5px;
+    }
+
+    p {
+        width: 35vw;
+        font-size: 26px;
+    }
+`
+export const Serviços = styled.div `
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 200px;
+
+    h1 {
+        font-size: 40px;
+        font-weight: normal;
+        font-family: 'Roboto Condensed', sans-serif;
+    }
+
+    hr {
+    width: 10%;
+    border: 0;
+    height: 1px;
+    background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(93, 166, 173, 0.75), rgba(0, 0, 0, 0));
+    }
+`
+export const Fileira= styled.div `
+    width: 100vw;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin: 100px 0;
+
+    img {
+        width: 50px;
+    }
+`
+export const Paises= styled.div `
+    display: flex;
+    flex-direction: column;
+
+    img {
+        width: 30px;
+    }
+`
+
